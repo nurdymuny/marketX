a8640d9b5ae329a60b49714253842e64db00bedf
Brian Russel Davis <brian@nurdymuny.com>
2016-03-03 16:01:43 +0530

    Imlemented animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,30 @@
-// import {Tab, Tabs} from 'react-toolbox';
-// var React = require('react')
-var ReactDOM = require('react-dom')
-var TabsExample = React.createClass(
-{
-// class TabsExample extends React.Component {
-  // state: {
-  //   index: 1
-  // }
+// // import {Tab, Tabs} from 'react-toolbox';
 
-  // handleTabChange: (index) => {
-  //   this.setState({index});
-  // }
+// var TabsExample = React.createClass(
+// {
+// // class TabsExample extends React.Component {
+//   // state: {
+//   //   index: 1
+//   // }
 
-  render: function () {
-    return (
-      <Tabs index={this.state.index} onChange={this.handleTabChange}>
-        <Tab label='Primary'><small>Primary content</small></Tab>
-        <Tab label='Secondary'><small>Secondary content</small></Tab>
-        <Tab label='Third' disabled><small>Disabled content</small></Tab>
-        <Tab label='Fourth' hidden><small>Fourth content hidden</small></Tab>
-        <Tab label='Fifth'><small>Fifth content</small></Tab>
-      </Tabs>
-    );
-  }
-});
+//   // handleTabChange: (index) => {
+//   //   this.setState({index});
+//   // }
 
-ReactDOM.renderComponent(
-  <TabsExample />,
-  document.getElementById('navigations')
-);
+//   render: function () {
+//     return (
+//       <Tabs index={this.state.index} onChange={this.handleTabChange}>
+//         <Tab label='Primary'><small>Primary content</small></Tab>
+//         <Tab label='Secondary'><small>Secondary content</small></Tab>
+//         <Tab label='Third' disabled><small>Disabled content</small></Tab>
+//         <Tab label='Fourth' hidden><small>Fourth content hidden</small></Tab>
+//         <Tab label='Fifth'><small>Fifth content</small></Tab>
+//       </Tabs>
+//     );
+//   }
+// });
+
+// ReactDOM.renderComponent(
+//   <TabsExample />,
+//   document.getElementById('navigations')
+// );
